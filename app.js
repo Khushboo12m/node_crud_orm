439e94cb0a0e50c2713309d525fec49861e350ce
Khushboo12m <khusboo.softrefine@gmail.com>
2025-06-30 13:00:54 +0530

    server

--- app.js
+++ app.js
@@ -30,5 +30,5 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(` Server is running on http://localhost:${PORT}`);
 });
